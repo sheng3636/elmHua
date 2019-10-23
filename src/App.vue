8061ba1635e8ef43104d2a378413f1324116728e
chensh <1789756569@qq.com>
2019-10-23 13:34:13 +0800

    feat(webpack.base.conf):为项目安装scss，并解决sass-loader版本过高，导致编译错误

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="outer">
+      <div class="inner"></div>
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -11,7 +14,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +22,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.outer{
+  position: relative;
+  margin:0 auto;
+  width:300px;
+  height: 300px;
+  background: #2c3e50;
+  .inner{
+    position: absolute;
+    margin-left: 50%;
+    margin-top: 50%;
+    transform: translate(-50%,-50%);
+    width: 200px;
+    height: 200px;
+    background: #fff;
+  }
 }
 </style>
